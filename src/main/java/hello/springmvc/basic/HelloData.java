417ca7b41559b1c23b72010a2823249425231cab
Dragonspine <ghoul300@naver.com>
2022-08-30 21:25:42 +0900

    feat: ModelAttribute

---
+++ src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,20 @@
+package hello.springmvc.basic;
+
+import lombok.Data;
+
+/**
+ * packageName    : hello.springmvc.basic
+ * fileName       : HelloData
+ * author         : kanghyun Kim
+ * date           : 2022/08/30
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/08/30        kanghyun Kim      최초 생성
+ */
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
